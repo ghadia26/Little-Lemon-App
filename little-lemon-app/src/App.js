37ed6819390804abd2fee71616a89e576381b31e
Ghadia <ghadianumaan@gmail.com>
2026-04-02 21:20:49 +0500

    Added navigation and brand colors

--- little-lemon-app/src/App.js
+++ little-lemon-app/src/App.js
@@ -2,6 +2,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <nav>
+  <ul>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Menu</a></li>
+    <li><a href="#">About</a></li>
+  </ul>
+</nav>
      <h1>Welcome to Little Lemon Restaurant!</h1>
     </div>
   );
